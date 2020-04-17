bd139c948f9597827fa4a02758ae5ba8f7f775a3
Chamod Shehanka <hcsperera@outlook.com>
2020-04-17 23:53:52 +0530

    Calendar Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { NavbarComponent } from './components/common/navbar/navbar.component';
 import { FooterComponent } from './components/common/footer/footer.component';
 import { CalendarComponent } from './components/calendar/calendar.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,7 @@ import { BannerComponent } from './components/banner/banner.component';
     CalendarComponent,
     BannerComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory })],
   providers: [],
   bootstrap: [AppComponent],
 })
